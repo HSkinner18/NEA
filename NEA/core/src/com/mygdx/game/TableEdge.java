d6bd06052c1736500fb6597437319914b59b10a5
HSkinner18 <148329257+HSkinner18@users.noreply.github.com>
2024-05-14 09:19:44 +0100

    Add files via upload

---
+++ NEA/core/src/com/mygdx/game/TableEdge.java
@@ -0,0 +1,65 @@
+
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class TableEdge {
+    private int w,h;
+    private int x,y;
+    private char plane;
+    private Color col;
+
+    public TableEdge(int x, int y, int w, int h, Color c){
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
+        col=c;
+    }
+    public void draw(ShapeRenderer sr){
+        sr.setColor(col);
+        sr.rect(x,y,w,h);
+    }
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+
+        this.y = y;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+
+    public Color getCol() {
+        return col;
+    }
+
+    public void setCol(Color col) {
+        this.col = col;
+    }
+}
